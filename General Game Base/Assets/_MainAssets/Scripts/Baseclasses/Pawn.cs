87837764a74aa1baab5c48ba6b56f624697548d4
LeeviPaa <leevi.paakkonen@hotmail.com>
2018-05-10 22:28:48 +0300

    fixed everything!

--- General Game Base/Assets/_MainAssets/Scripts/Baseclasses/Pawn.cs
+++ General Game Base/Assets/_MainAssets/Scripts/Baseclasses/Pawn.cs
@@ -5,34 +5,17 @@ using UnityEngine;
 /// <summary>
 /// This is a class that the player controller can possess and unposses to interact with the world
 /// </summary>
-[RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(PhotonView))]
 public class Pawn : Photon.MonoBehaviour {
 
     public bool Possessable = true;
-    public float moveSpeed = 20;
-    public float rotSpeed = 1;
-    [Range(0,1)]
-    public float wallSpeed = 0.5f;
-    [HideInInspector]
-    public float slideRayMagnitude = 2;
-    public LayerMask collisionLayer;
-    public LayerMask networkLayer;
 
     //private
     private bool possessed = false;
     protected PhotonView pv;
-    private Rigidbody rb;
-
-    private Ray r;
-    private RaycastHit hit;
-    private CapsuleCollider thisCollider;
 
     protected virtual void Awake()
     {
-        thisCollider = GetComponentInChildren<CapsuleCollider>();
-
-        rb = transform.GetOrAddComponent<Rigidbody>();
         pv = transform.GetOrAddComponent<PhotonView>();
         if (!pv.isMine)
         {
@@ -72,94 +55,6 @@ public class Pawn : Photon.MonoBehaviour {
         }
         //rb.isKinematic = true;
         //thisCollider.enabled = false;
-        gameObject.layer = layermask_to_layer(networkLayer);
     }
 
-    private int layermask_to_layer(LayerMask layerMask)
-    {
-        int layerNumber = 0;
-        int layer = layerMask.value;
-        while (layer > 0)
-        {
-            layer = layer >> 1;
-            layerNumber++;
-        }
-        return layerNumber - 1;
-    }
-
-    protected void MoveDirection(Vector3 direction)
-    {
-        if (thisCollider != null && rb != null)
-        {
-            Vector3 moveVector = direction * moveSpeed * Time.deltaTime;
-
-            //disable the collider because it would otherwise get hit with the capsule casts and checks
-            thisCollider.enabled = false;
-
-            //capsule cast positions
-            Vector3 P1 = thisCollider.center + thisCollider.transform.position - (Vector3.up * (thisCollider.height * 0));
-            Vector3 P2 = thisCollider.center + thisCollider.transform.position + (Vector3.up * (thisCollider.height / 4));
-            //Debug.DrawLine(P1, P2);
-
-            Vector3 P3 = thisCollider.center + thisCollider.transform.position - (Vector3.up * (thisCollider.height * 0));
-            P3 += moveVector;
-            Vector3 P4 = thisCollider.center + thisCollider.transform.position + (Vector3.up * (thisCollider.height / 4));
-            P4 += moveVector;
-            //Debug.DrawLine(P3, P4, Color.red);
-
-            //check with the specs of the current collider if there is something in the way
-            if (!Physics.CapsuleCast(P1, P2, thisCollider.radius * 1.0f, moveVector, out hit, moveVector.magnitude, collisionLayer, QueryTriggerInteraction.Ignore) &&
-                !Physics.CheckCapsule(P3, P4, thisCollider.radius * 1.0f, collisionLayer, QueryTriggerInteraction.Ignore))
-            {
-                //Enable the collider for further non input related collision
-                thisCollider.enabled = true;
-                rb.MovePosition(transform.position + moveVector);
-            }
-            else
-            {
-                //something is in the way, correcting movement
-                if(Physics.Raycast(transform.position, moveVector, out hit, moveVector.magnitude+thisCollider.radius *slideRayMagnitude,  collisionLayer, QueryTriggerInteraction.Ignore))
-                {
-                    //check with a ray if there is a collider close that we can get a position to move towards slightly
-                    Vector3 moveVectorClose = (hit.point + (hit.normal*thisCollider.radius));
-
-                    ////check if point is available because we can get jerky in corners
-                    //P3 = moveVectorClose;
-                    //P4 = moveVectorClose + (Vector3.up * (thisCollider.height / 4));
-
-                    //Debug.DrawLine(P3, P4, Color.red);
-                    //if (!Physics.CheckCapsule(P3, P4, thisCollider.radius * 0.9f, collisionLayer, QueryTriggerInteraction.Ignore))
-                    //{
-
-                    //}
-
-                    rb.MovePosition(Vector3.MoveTowards(transform.position, moveVectorClose, moveSpeed * wallSpeed * Time.deltaTime));
-                    //Debug.Log("ray move");
-
-                }
-                else
-                {
-                    //ray did not hit so we move check left and right from the next position
-                    if(Physics.Raycast(transform.position + moveVector, transform.right, out hit, thisCollider.radius, collisionLayer, QueryTriggerInteraction.Ignore))
-                    {
-                        Vector3 moveVectorClose = (hit.point + (hit.normal * thisCollider.radius));
-                        rb.MovePosition(Vector3.MoveTowards(transform.position, moveVectorClose, moveSpeed * wallSpeed * Time.deltaTime));
-                    }
-                    else if(Physics.Raycast(transform.position + moveVector, -transform.right, out hit, thisCollider.radius, collisionLayer, QueryTriggerInteraction.Ignore))
-                    {
-                        Vector3 moveVectorClose = (hit.point + (hit.normal * thisCollider.radius));
-                        rb.MovePosition(Vector3.MoveTowards(transform.position, moveVectorClose, moveSpeed * wallSpeed * Time.deltaTime));
-                    }
-                }
-            }
-            //enable the collider whatever happens
-            thisCollider.enabled = true;
-        }
-
-    }
-
-    protected void RotateYDirection(float rotation)
-    {
-        rb.MoveRotation(transform.rotation*Quaternion.Euler(0, rotation*Time.deltaTime*rotSpeed, 0));
-    }
 }
--- General Game Base/Assets/_MainAssets/Scripts/Gamemode1/TestPawn.cs
+++ General Game Base/Assets/_MainAssets/Scripts/Gamemode1/TestPawn.cs
@@ -4,12 +4,76 @@ using UnityEngine;
 
 public class TestPawn : Pawn
 {
+    CharacterController pc;
+    private Vector3 currVelocity = Vector3.zero;
+
+    //Jumping
+    public float jumpSpeed = 10;
+    private float currJumpSpeed = 0;
+
+    //gravity
+    public float gravityAcceleration = 10;
+    public float maxFallSpeed = 10;
+    private float gravity = 1;
+
+    //movement
+    public float aerialSpeedMod = 0.4f;
+    public float speed = 10;
+    public float rotationSpeed = 10;
+
+    private void Start()
+    {
+        pc = GetComponent<CharacterController>();
+    }
     private void Update()
     {
         //if this is not mine, don't do anything!
         if(!pv.isMine){ return; }
 
-        MoveDirection((transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Rotation")));
-        RotateYDirection(Input.GetAxis("Horizontal"));
+        CalculateAccelerations();
+        CalculateVelocity();
+
+        transform.Rotate(new Vector3(0, Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime, 0));
+
+        pc.Move(currVelocity* Time.deltaTime);
+        
+    }
+    private void CalculateAccelerations()
+    {
+        if(!pc.isGrounded )
+        {
+            if(Mathf.Abs(gravity) < maxFallSpeed)
+                gravity += gravityAcceleration * Time.deltaTime;
+        }
+        else
+        {
+            gravity = 0;
+        }
+
+        if(currJumpSpeed > 0)
+        {
+            currJumpSpeed -= gravityAcceleration * Time.deltaTime;
+        }
+        else
+        {
+            currJumpSpeed = 0;
+        }
+    }
+    private void CalculateVelocity()
+    {
+        currVelocity = (transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Rotation"));
+        currVelocity *= speed;
+        currVelocity += Vector3.up * currJumpSpeed;
+
+        if (!pc.isGrounded)
+        {
+            currVelocity -= Vector3.up * gravity;
+            currVelocity *= aerialSpeedMod;
+        }
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            currJumpSpeed = jumpSpeed;
+        }
     }
 }
